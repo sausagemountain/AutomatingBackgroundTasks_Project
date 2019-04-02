557022976345019f2e3288d5af24b17a4e6179f3
miha2158 <miha.vanya@gmail.com>
2019-04-02 12:42:39 +0500

    removed custom main

--- AutomatingBackgroundTasks.Interface/App.xaml.cs
+++ AutomatingBackgroundTasks.Interface/App.xaml.cs
@@ -16,48 +16,13 @@ namespace AutomatingBackgroundTasks.Interface
     /// </summary>
     public partial class App : Application
     {
-        [STAThread]
-        static void Main()
-        {
-            if (!MessageQueue.Exists(QueueName)) {
-                queue = MessageQueue.Create(QueueName);
-            }
-            else {
-                queue = new MessageQueue(QueueName);
-            }
-            queue.Formatter = new XmlMessageFormatter(new[] { typeof(string) });
-
-            if (mutex.WaitOne(TimeSpan.Zero, true))
-            {
-                App app = new App();
-                app.MainWindow = new WindowMain();
-                app.Run(app.MainWindow);
-                mutex.ReleaseMutex();
-            }
-            else
-            {
-                try {
-                    queue.Send(AppearMessage, AppName);
-                }
-                finally {
-                }
-            }
-            queue.Close();
-        }
-
-        App() {
-            InitializeComponent();
-        }
-
-        public bool isRunning = true;
-
         public void ReceiveQueueMessage()
         {
-            queue.Refresh();
-            var msg = queue.Receive();
+            _queue.Refresh();
+            var msg = _queue.Receive();
             if (msg == null)
                 return;
-            msg.Formatter = queue.Formatter;
+            msg.Formatter = _queue.Formatter;
 
             if (msg.Label == AppName && (msg.Body as string) == AppearMessage)
             {
@@ -75,27 +40,42 @@ namespace AutomatingBackgroundTasks.Interface
         private static readonly string AppName = "Automating Background Tasks";
         public static readonly string QueueName = $@".\private$\{AppName.Replace(" ", "-")}-Queue";
         private static readonly string AppearMessage = "Appear!";
-        public static MessageQueue queue = null;
 
-        private static Mutex mutex = new Mutex(true, AppName);
+        private static MessageQueue _queue = null;
+        private static Mutex _mutex = null;
+
+        bool _isRunning = true;
+        bool _createdNew;
+        Thread _messageGetter;
 
         private void this_Startup(object sender, StartupEventArgs e)
         {
+            _queue = !MessageQueue.Exists(QueueName) ? MessageQueue.Create(QueueName) : new MessageQueue(QueueName);
+            _queue.Formatter = new XmlMessageFormatter(new[] { typeof(string) });
+
+            _mutex = new Mutex(true, AppName, out _createdNew);
+            if (!_createdNew) {
+                _queue.Send(AppearMessage, AppName);
+                Shutdown();
+                return;
+            }
+
             Preferences.Default.ToString();
             
-            messageGetter = new Thread(() => {
-                while (isRunning)
+            _messageGetter = new Thread(() => {
+                while (_isRunning)
                     ReceiveQueueMessage();
             });
-            messageGetter.IsBackground = true;
-            messageGetter.Start();
+            _messageGetter.IsBackground = true;
+            _messageGetter.Start();
         }
-        Thread messageGetter;
-
+        
         private void this_Exit(object sender, ExitEventArgs e)
         {
-            isRunning = false;
-            queue.Send(null, string.Empty);
+            _isRunning = false;
+            if(_createdNew)
+                _queue.Send(null, string.Empty);
+            _queue.Close();
         }
     }
 }
